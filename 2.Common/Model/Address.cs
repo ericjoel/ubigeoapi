79e14f561b53a4f830abef401fcd0bd94bb6f5cf
ericjoel <eric.contreras93@gmail.com>
2018-06-06 12:43:10 -0500

    Generic Repository and Model Address and Police with their own repository ¡

---
+++ 2.Common/Model/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Model
+{
+    public class Address
+    {
+        public string Department { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+    }
+}
---
+++ 2.Common/Model/Police.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Common.Model
+{
+    public class Police
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Address Address { get; set; }
+    }
+}
