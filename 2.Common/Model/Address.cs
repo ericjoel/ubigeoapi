859f0e74b9ef50c98bf2dc069c705cc73d4cf1ff
ericjoel <eric.contreras93@gmail.com>
2018-06-07 09:34:36 -0500

    Modifying address and police class with validates correctly

--- 2.Common/Model/Address.cs
+++ 2.Common/Model/Address.cs
@@ -1,9 +1,13 @@
-﻿namespace Common.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.Model
 {
     public class Address
     {
         public string Department { get; set; }
-        public string Province { get; set; }
+        
         public string District { get; set; }
+        
+        public string Province { get; set; }        
     }
 }
--- 2.Common/Model/Police.cs
+++ 2.Common/Model/Police.cs
@@ -1,11 +1,46 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Common.Model
 {
-    public class Police
+    public class Police : IValidatableObject
     {
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public Address Address { get; set; }
+        [BsonElement("_id")]
+        public ObjectId Id { get; set; }
+
+        public Address Address { get; set; }        
+
+        public DateTime? EndDate { get; set; }
+
+        [Required(ErrorMessage ="Start date is required")]
+        public DateTime? StartDate { get; set; }
+
+        [Required(ErrorMessage = "Department is required")]
+        [StringLength(20, ErrorMessage = "Department exceeds max length (20)")]
+        [BsonIgnore]
+        public string IdDepartment { get; set; }
+
+        [Required(ErrorMessage = "District is required")]
+        [StringLength(100, ErrorMessage = "District exceeds max length (20)")]
+        [BsonIgnore]
+        public string IdDistrict { get; set; }
+
+        [Required(ErrorMessage = "Province is required")]
+        [StringLength(100, ErrorMessage = "Province exceeds max length (20)")]
+        [BsonIgnore]
+        public string IdProvince { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            if (StartDate > DateTime.Now)
+                results.Add(new ValidationResult("The start date must be less than today"));
+            
+            return results;
+        }
     }
 }
