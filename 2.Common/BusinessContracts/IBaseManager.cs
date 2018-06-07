77b62e553f9c23c60276012b67cfdc017915c39d
ericjoel <eric.contreras93@gmail.com>
2018-06-07 09:35:09 -0500

    Adding manager contracts

---
+++ 2.Common/BusinessContracts/IBaseManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Common.BusinessContracts
+{
+    public interface IBaseManager<TEntity>
+        where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> PageAll(int skip, int take);
+
+        TEntity FindById(object id);
+
+        TEntity Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ 2.Common/BusinessContracts/IPoliceManager.cs
@@ -0,0 +1,10 @@
+﻿using Common.Model;
+
+namespace Common.BusinessContracts
+{
+    public interface IPoliceManager : IBaseManager<Police>
+    {
+        string GetNameDistrictById(string id);
+        string GetNameProvinceById(string id);
+    }
+}
