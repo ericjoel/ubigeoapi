6551ab34ca1706f50bd85a02815e62ee68c7b8b8
ericjoel <eric.contreras93@gmail.com>
2018-06-07 22:03:09 -0500

    comment repsitories

--- 0.DataAccess/ProvinceRepository.cs
+++ 0.DataAccess/ProvinceRepository.cs
@@ -1,18 +1,41 @@
-﻿using Common.DataAccessContracts;
+﻿using System.Collections.Generic;
+using Common.DataAccessContracts;
 using Common.Model;
 using MongoDB.Driver;
 
 namespace DataAccess
 {
+    /// <summary>
+    /// Class that access province repository in mongo
+    /// </summary>
     public class ProvinceRepository : Repository<Province>, IProvinceRepository
     {
+        /// <summary>
+        /// Construct province repository
+        /// </summary>
+        /// <param name="mongoClient">Inject mongloclient</param>
         public ProvinceRepository(IMongoClient mongoClient) : base(mongoClient)
         {
         }
 
+        /// <summary>
+        /// Find a province by specified id
+        /// </summary>
+        /// <param name="id">id to search</param>
+        /// <returns>the province searched</returns>
         public override Province FindById(object id)
         {
             return Collection.Find(p => p.Id.Equals(id)).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Find provinces by an specified department
+        /// </summary>
+        /// <param name="idDepartment">id department</param>
+        /// <returns>provinces searched</returns>
+        public IEnumerable<Province> GetByIdDepartment(string idDepartment)
+        {
+            return Collection.Find(p => p.IdDepartment.Equals(idDepartment)).ToEnumerable();
         }
     }
 }
--- 0.DataAccess/Repository.cs
+++ 0.DataAccess/Repository.cs
@@ -1,36 +1,56 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading;
-using System.Threading.Tasks;
 using Common.DataAccessContracts;
 using MongoDB.Driver;
 using System.Linq;
 
 namespace DataAccess
 {
+    /// <summary>
+    /// Implement of generic repository, to avoid implement in multiple classes
+    /// </summary>
+    /// <typeparam name="TEntity">Generic model</typeparam>
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
         private IMongoClient _client;
         private IMongoDatabase _mongoDataBase;
         protected IMongoCollection<TEntity> Collection;
 
+        /// <summary>
+        /// Construct generic repository
+        /// </summary>
+        /// <param name="mongoClient">Inject mongoclient</param>
         public Repository(IMongoClient mongoClient)
         {
             _client = mongoClient;
+            //Specified the database to use in project
             _mongoDataBase = mongoClient.GetDatabase("PoliceStore");
+            //Assign the collection by class name
             Collection = _mongoDataBase.GetCollection<TEntity>(typeof(TEntity).Name);            
         }
-
+        /// <summary>
+        /// Generic method to get all registers in collection
+        /// </summary>
+        /// <returns>list of models</returns>
         public IEnumerable<TEntity> GetAll()
         {
             return Collection.Find(_ => true).ToEnumerable();
         }
 
+        /// <summary>
+        /// Page all the model 
+        /// </summary>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns></returns>
         public IEnumerable<TEntity> PageAll(int skip, int take)
         {
             return Collection.AsQueryable().Where(_ => true).Take(take).Skip(skip).ToList();
         }       
-
+        /// <summary>
+        /// Add a register in a collection, specified in TEntity class
+        /// </summary>
+        /// <param name="entity">model to insert</param>
+        /// <returns>returns the register inserted</returns>
         public TEntity Add(TEntity entity)
         {
             Collection.InsertOne(entity);
