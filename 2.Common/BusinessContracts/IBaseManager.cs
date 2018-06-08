fb7853aba998b0efc6f15773570d1c9bd1a7ae3d
ericjoel <eric.contreras93@gmail.com>
2018-06-07 22:02:30 -0500

    commentiing bas and police manger

--- 2.Common/BusinessContracts/IBaseManager.cs
+++ 2.Common/BusinessContracts/IBaseManager.cs
@@ -2,14 +2,32 @@
 
 namespace Common.BusinessContracts
 {
+    /// <summary>
+    /// Base Manager interface
+    /// </summary>
+    /// <typeparam name="TEntity">Model Generic passed by class</typeparam>
     public interface IBaseManager<TEntity>
         where TEntity : class
     {
+        /// <summary>
+        /// Get all data of an specified model
+        /// </summary>
+        /// <returns>All models</returns>
         IEnumerable<TEntity> GetAll();
         IEnumerable<TEntity> PageAll(int skip, int take);
-
+        
+        /// <summary>
+        /// Get a model by specified id
+        /// </summary>
+        /// <param name="id">if to search</param>
+        /// <returns>Model searched or null</returns>
         TEntity FindById(object id);
 
+        /// <summary>
+        /// Add an specified model 
+        /// </summary>
+        /// <param name="entity">model to insert</param>
+        /// <returns>model with id</returns>
         TEntity Add(TEntity entity);
         void Update(TEntity entity);
         void Remove(TEntity entity);
--- 2.Common/BusinessContracts/IPoliceManager.cs
+++ 2.Common/BusinessContracts/IPoliceManager.cs
@@ -2,9 +2,22 @@
 
 namespace Common.BusinessContracts
 {
+    /// <summary>
+    /// Inherit IBaseManager and can specified get specifis methods
+    /// </summary>
     public interface IPoliceManager : IBaseManager<Police>
     {
+        /// <summary>
+        /// Get name of district by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         string GetNameDistrictById(string id);
+        /// <summary>
+        /// Get name of province by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         string GetNameProvinceById(string id);
     }
 }
